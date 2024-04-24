17dd8d34787dbada7a721b96fba2806ffafff9e1
Josh <josh.mayfield45@gmail.com>
2024-04-24 15:05:01 -0400

    updated index.js in models folder

--- models/index.js
+++ models/index.js
@@ -5,5 +5,35 @@ const Comment = require('../models/Comment');
 // create relationship between users and blogposts
 User.hasMany(BlogPost, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
 });
+
+// create relationship between users and comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+  });
+
+// use belongsto to connect blogposts to users
+BlogPost.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// create relationship between blogposts and comments
+BlogPost.hasMany(Comment, {
+    foreignKey: 'blog_id',
+  });
+
+// use belongsTo to connect comments to users
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// use belongsTo to connect comments to blogposts
+Comment.belongsTo(BlogPost, {
+  foreignKey: 'blog_id'
+});
+
+module.exports = {
+  User,
+  BlogPost,
+  Comment
+};
