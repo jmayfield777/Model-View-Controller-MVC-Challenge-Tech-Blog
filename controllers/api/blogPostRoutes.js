db5a38aed651b30b0e2f4c3a1efda0c1d28225bc
Josh <josh.mayfield45@gmail.com>
2024-04-24 16:38:58 -0400

    updated blogPostRoutes.js

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -55,7 +55,7 @@ router.get('/:id', async (req, res) => {
     if (!blogData) {
       res
         .status(404)
-        .json({ message: 'No post found with this id' });
+        .json({ message: 'No blogpost found with this id' });
       return;
     }
 
@@ -64,3 +64,69 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// create a blogpost
+router.post('/', async (req, res) => {
+  try {
+    const blogData = await BlogPost.create({
+        blog_title: req.body.blog_title,
+        blog_text: req.body.blog_text,
+        user_id: req.session.user_id
+    });
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// update a blogpost
+router.put('/:id', async (req, res) => {
+  try {
+    const blogData = await BlogPost.update(
+    {
+      blog_title: req.body.blog_title,
+      blog_text: req.body.blog_text,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!blogData) {
+      res
+        .status(404)
+        .json({ message: 'No blogpost found with this id' });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete a blogpost
+router.delete('/:id', async (req, res) => {
+  try {
+    const blogData = await BlogPost.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!blogData) {
+      res
+        .status(404)
+        .json({ message: 'No blogpost found with this id' });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
