9dac4f432831ca431ba20717694c136a647fed86
Josh <josh.mayfield45@gmail.com>
2024-04-25 15:56:39 -0400

    added delete route to commentRoutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, BlogPost, Comment } = require('../../models');
+const withAuth = require('../../utils/withAuth');
 
 // get all comments
 router.get('/', async (req, res) => {
@@ -22,6 +23,29 @@ router.post('/', async (req, res) => {
    });
 
    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete a comment
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const comments = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!comments) {
+      res
+        .status(404)
+        .json({ message: 'Not found' });
+      return;
+    }
+
+    res.status(200).json(comments);
   } catch (err) {
     res.status(500).json(err);
   }
