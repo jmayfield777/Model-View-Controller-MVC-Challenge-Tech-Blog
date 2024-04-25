9756dee05def7489fd83d34e90c4d766afbf5972
Josh <josh.mayfield45@gmail.com>
2024-04-25 15:00:18 -0400

    added logoutHandler.js and updated main.handlebars to grab elements in the handler

---
+++ public/js/logoutHandler.js
@@ -0,0 +1,14 @@
+const logoutHandler = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.status(500));
+  }
+};
+
+document.querySelector('#logout-button').addEventListener('click', logoutHandler);
