e2b6efc55568b9cc8f2ec5635adb74789483dd2e
Josh <josh.mayfield45@gmail.com>
2024-04-25 15:46:32 -0400

    added homeRoutes.js and updated blogPostRoutes.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const { User, BlogPost, Comment } = require('../models');
+const withAuth = require('../utils/withAuth');
+
+// get all blogpost
+router.get('/', async (req, res) => {
+  try {
+    const blogData = await BlogPost.findAll({
+      include: [{
+        model: User,
+        attributes: ['name'],
+      }],
+    });
+
+    const blogs = blogData.map((blog) => blog.get({
+      plain: true,
+    }));
+
+    res.render('homepage', {
+      blogs,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+  
+// get one blogpost
+router.get('/blogpost/:id', async (req, res) => {
+  try {
+    const blogData = await BlogPost.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Comment,
+          include: [
+            User
+          ],
+        },
+      ],
+    });
+
+    const blog = blogData.get({
+      plain: true
+    });
+
+    res.render('blogPost', {
+      ...blog,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// dashboard route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: {
+        exclude: ['password']
+      },
+        include: [{
+          model: BlogPost
+        }],
+    });
+
+    const loggedInUser = userData.get({
+      plain: true,
+    });
+
+    res.render('dash', {
+      ...loggedInUser,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
