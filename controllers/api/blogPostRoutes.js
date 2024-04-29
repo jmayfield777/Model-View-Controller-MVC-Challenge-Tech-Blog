4b0603ea4f9b06f2e4f51e37613fcb15916ce6ab
Josh <josh.mayfield45@gmail.com>
2024-04-29 15:37:10 -0400

    updated blogPostRoutes.js

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -7,37 +7,9 @@ const withAuth = require('../../utils/withAuth');
 router.post('/', withAuth, async (req, res) => {
   try {
     const blogData = await BlogPost.create({
-        blog_title: req.body.blog_title,
-        blog_text: req.body.blog_text,
+        ...req.body,
         user_id: req.session.user_id
     });
-
-    res.status(200).json(blogData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// update a blogpost
-router.put('/:id', withAuth, async (req, res) => {
-  try {
-    const blogData = await BlogPost.update(
-    {
-      blog_title: req.body.blog_title,
-      blog_text: req.body.blog_text,
-    },
-    {
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!blogData) {
-      res
-        .status(404)
-        .json({ message: 'No blogpost found with this id' });
-      return;
-    }
 
     res.status(200).json(blogData);
   } catch (err) {
